13e6fffbf6e2c1fcf815417da51aa08c3231ffc0
hmmpryt <hamampriyatmoko81@gmail.com>
2024-04-03 21:59:04 +0700

    Update HomeView to display list of contacts using FutureBuilder

--- lib/Screen/home_view.dart
+++ lib/Screen/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:data_kontak/controller/kontak_controller.dart';
+import 'package:data_kontak/model/kontak.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -20,7 +21,33 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Daftar Orang')),
+      appBar: AppBar(
+        title: Text('Daftar Orang'),
+      ),
+      body: FutureBuilder<List<Kontak>>(
+        future: _controller.getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Kontak person = snapshot.data![index];
+                return ListTile(
+                  title: Text(person.nama),
+                  subtitle: Text(person.email),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(person.foto),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
