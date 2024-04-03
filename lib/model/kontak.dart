791bd4a0878f13845b190dbb6ec321271bf449dc
hmmpryt <hamampriyatmoko81@gmail.com>
2024-04-03 20:46:33 +0700

    menambahkan properti

--- lib/model/kontak.dart
+++ lib/model/kontak.dart
@@ -1,3 +1,14 @@
-class Kontak{
-  
+class Kontak {
+  final String nama;
+  final String email;
+  final String alamat;
+  final String telepon;
+  final String foto;
+  Kontak({
+    required this.nama,
+    required this.telepon,
+    required this.email,
+    required this.alamat,
+    required this.foto,
+  });
 }
