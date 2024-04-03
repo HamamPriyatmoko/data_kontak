a0d1a562704c9dae564ef28c74dfc7041c3b3935
hmmpryt <hamampriyatmoko81@gmail.com>
2024-04-03 20:00:04 +0700

    tambah method uri

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,4 +1,9 @@
 class KontakService {
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endPoint = 'people';
+
+  Uri getUri(String path) {
+    return Uri.parse("$baseUrl$path");
+  }
+  
 }
